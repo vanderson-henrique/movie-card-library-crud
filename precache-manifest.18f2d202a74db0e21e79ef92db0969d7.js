276bf5f36a5521535f0d78c7386519c488b4b870
vanderson-henrique <vhbenedito@gmail.com>
2021-02-06 18:18:33 -0300

    Updates

---
+++ precache-manifest.18f2d202a74db0e21e79ef92db0969d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "621c710fdfa2871cf96428993059b3fb",
+    "url": "/app-movie-cards-library-crud/index.html"
+  },
+  {
+    "revision": "ee0a36aa6af8c5d8e3bc",
+    "url": "/app-movie-cards-library-crud/static/css/main.94935c5c.chunk.css"
+  },
+  {
+    "revision": "4e81a2196b90644681b4",
+    "url": "/app-movie-cards-library-crud/static/js/2.af0f5ade.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/app-movie-cards-library-crud/static/js/2.af0f5ade.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee0a36aa6af8c5d8e3bc",
+    "url": "/app-movie-cards-library-crud/static/js/main.b2a1a78f.chunk.js"
+  },
+  {
+    "revision": "5a06dfdfb3a64ed39296",
+    "url": "/app-movie-cards-library-crud/static/js/runtime-main.daa33e36.js"
+  }
+]);
